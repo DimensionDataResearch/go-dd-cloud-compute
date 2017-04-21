bef11f8b0bad446bb036c59eae98388832e2857a
Adam Friedman <tintoy@tintoy.io>
2017-04-21 16:55:59 +1000

    Move constants into a single file.

--- compute/servers.go
+++ compute/servers.go
@@ -8,48 +8,6 @@ import (
 	"net/url"
 )
 
-const (
-	// NetworkAdapterTypeE1000 represents the E1000 network adapter type.
-	NetworkAdapterTypeE1000 = "E1000"
-
-	// NetworkAdapterTypeVMXNET3 represents the VMXNET3 network adapter type.
-	NetworkAdapterTypeVMXNET3 = "VMXNET3"
-
-	// NetworkAdapterTypeE1000E represents the E1000e network adapter type.
-	NetworkAdapterTypeE1000E = "E1000E"
-
-	// NetworkAdapterTypeEnhancedVMXNET2 represents the VMXNET2/Enhanced network adapter type.
-	NetworkAdapterTypeEnhancedVMXNET2 = "ENHANCED_VMXNET2"
-
-	// NetworkAdapterTypeFlexiblePCNET32 represents the PCNET32/Flexible network adapter type.
-	NetworkAdapterTypeFlexiblePCNET32 = "FLEXIBLE_PCNET32"
-)
-
-const (
-	// StorageControllerAdapterTypeBusLogicParallel represents the BusLogic Parallel storage controller adapter type.
-	StorageControllerAdapterTypeBusLogicParallel = "BUSLOGIC_PARALLEL"
-
-	// StorageControllerAdapterTypeLSILogicParallel represents the LSI Logic Parallel storage controller adapter type.
-	StorageControllerAdapterTypeLSILogicParallel = "LSI_LOGIC_PARALLEL"
-
-	// StorageControllerAdapterTypeLSILogicSAS represents the LSI Logic SAS storage controller adapter type.
-	StorageControllerAdapterTypeLSILogicSAS = "LSI_LOGIC_SAS"
-
-	// StorageControllerAdapterTypeEnhancedVMWareParavirtual represents the VMWare Paravirtual storage controller adapter type.
-	StorageControllerAdapterTypeEnhancedVMWareParavirtual = "VMWARE_PARAVIRTUAL"
-)
-
-const (
-	// ServerDiskSpeedEconomy represents the economy speed for server disks.
-	ServerDiskSpeedEconomy = "ECONOMY"
-
-	// ServerDiskSpeedStandard represents the standard speed for server disks.
-	ServerDiskSpeedStandard = "STANDARD"
-
-	// ServerDiskSpeedHighPerformance represents the high-performance speed for server disks.
-	ServerDiskSpeedHighPerformance = "HIGHPERFORMANCE"
-)
-
 // Server represents a virtual machine.
 type Server struct {
 	ID              string                        `json:"id"`
